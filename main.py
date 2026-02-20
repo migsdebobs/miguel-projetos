82103016bd4051eb7fb8a7e3284f0133f42620a6
migsdebobs <miguelinhares081@gmail.com>
2026-02-19 21:36:11 -0300

    aula 3 criação de classe

---
+++ main.py
@@ -0,0 +1,20 @@
+'''SpiceFlix
+
+Cliente_usuario
+Assinaturas_usuario 
+
+O que faz :
+1	Cliente_usuario:
+Senha_usuario
+Email_usuario
+Telefone_usuario
+Cpf_usuario
+Login_usuario
+Logout
+Recuperar_senha
+
+2	Assinaturas 
+Filmes_extras
+Planos_1, plano_2 '''
+
+
